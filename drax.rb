94f23f6bf721899e8dae2d4ba555d6303328bf93
draxsoft <frankkostine@gmail.com>
2024-07-11 18:36:10 -0500

    Update drax.rb

--- drax.rb
+++ drax.rb
@@ -2,7 +2,7 @@ class Drax < Formula
   desc "A command-line tool for various game settings"
   homepage "https://github.com/draxsoft/drax"
   url "https://github.com/draxsoft/drax/archive/refs/tags/v1.0.0.zip"
-  sha256 "710737cfb9cbf47c378a6966133438ba45379bee1f2d005d38cdeede11322933"
+  sha256 "a8982734c77c0bf5274c9b8436217376ece3a9316e6c04c0cb80694c3d00031d"
 
   depends_on :xcode => ["12.0", :build]
 
