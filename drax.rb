4bfc717e065450d302a3761220d60da0b9bc834a
draxsoft <frankkostine@gmail.com>
2024-07-11 21:03:07 -0500

    Update drax.rb

--- drax.rb
+++ drax.rb
@@ -1,9 +1,9 @@
 class Drax < Formula
   desc "Swift command-line tool for [description of drax]"
   homepage "https://github.com/draxsoft/drax"
-  url "https://github.com/draxsoft/drax/releases/download/v2.0.0/v2.0.0.zip"
-  sha256 "10d33b9133532905d54efad6e4bf3416253b5144885ead5061433d94f06fa04f"
-  version "1.0.0"
+  url "https://github.com/draxsoft/drax/releases/download/v2.0.0/2.0.0.zip"
+  sha256 "50582c9ba0d570a3ab29a3eb83c2650a9f2cb1ef02727243e06ee6141190264a"
+  version "2.0.0"
 
   depends_on :xcode => ["12.0", :build]
 
