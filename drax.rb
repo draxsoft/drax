4e52f0b248b48d6223818f97c5f106fc546621ae
draxsoft <frankkostine@gmail.com>
2024-07-11 19:03:15 -0500

    Update drax.rb

--- drax.rb
+++ drax.rb
@@ -2,7 +2,7 @@ class Drax < Formula
   desc "A command-line tool for various game settings"
   homepage "https://github.com/draxsoft/drax"
   url "https://github.com/draxsoft/drax/releases/download/v1.0.0/v1.0.0.zip"
-  sha256 "10d33b9133532905d54efad6e4bf3416253b5144885ead5061433d94f06fa04f"
+  sha256 "your_sha256_checksum_here"
   depends_on :xcode => ["12.0", :build]
 
   def install
@@ -16,16 +16,8 @@ class Drax < Formula
       extracted_dir = Dir.glob("#{tmpdir}/*").first
       raise "Unable to find extracted directory" unless extracted_dir && File.directory?(extracted_dir)
 
-      # Copy all contents from the extracted directory to the current working directory
-      cp_r "#{extracted_dir}/.", "."
-
       # Change directory to the extracted folder
       cd extracted_dir do
-        # Check if Package.swift exists in the correct directory
-        unless File.exist?("Package.swift")
-          raise "Package.swift not found. Aborting."
-        end
-
         # Build the project
         system "swift", "build", "-c", "release", "--disable-sandbox"
 
